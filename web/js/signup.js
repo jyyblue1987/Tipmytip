01241bfcd9e76542031b858322ee827b84043ad9
JKH <jyyblue1987@outlook.com>
2016-09-09 01:53:24 +0800

    [add] add signup page layout.

--- web/js/signup.js
+++ web/js/signup.js
@@ -6,5 +6,36 @@
 
 $( document ).ready(function() {
 
+    $('.formbox').validate({
+        rules: {
+            email: {
+                required: true
+            },
+            password: {
+                required: true
+            }
+        },
+        highlight: function(element) {
+            var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            $(id_attr).removeClass('glyphicon-ok').addClass('glyphicon-remove');
+        },
+        unhighlight: function(element) {
+            var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(id_attr).removeClass('glyphicon-remove').addClass('glyphicon-ok');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.length) {
+                error.insertAfter(element);
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
+    $('#date_of_birth').datepicker();
 
 });
