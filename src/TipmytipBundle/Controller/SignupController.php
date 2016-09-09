134896950aab01da0ea58c75fd015cea33e8cbe6
JKH <jyyblue1987@outlook.com>
2016-09-10 01:10:56 +0800

    [add] add country and city list.

--- src/TipmytipBundle/Controller/SignupController.php
+++ src/TipmytipBundle/Controller/SignupController.php
@@ -4,10 +4,8 @@ namespace TipmytipBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use TipmytipBundle\Entity\User;
-use TipmytipBundle\Form\UserType;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * Signup controller.
@@ -19,10 +17,43 @@ class SignupController extends Controller
 
     public function indexAction(Request $request)
     {
-
         return $this->render('signup/index.html.twig', array(
-            
+
         ));
+    }
+
+    /**
+     * @Route("/signup/location")
+     */
+
+    public function locationAction(Request $request) {
+        $sql = "SELECT * FROM country";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $countrylist = $stmt->fetchAll();
+
+        $ret = array();
+        $ret['countrylist'] = $countrylist;
+
+        return $this->json($ret);
+    }
+
+    public function cityAction($country_id) {
+        $sql = sprintf("SELECT * FROM location where country_id = %s", $country_id);
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $citylist = $stmt->fetchAll();
+
+        $ret = array();
+        $ret['citylist'] = $citylist;
+
+        return $this->json($ret);
     }
 
 }
--- web/js/signup.js
+++ web/js/signup.js
@@ -3,12 +3,41 @@ app = angular.module('app',    [
     ]);
 
 app.controller('SignUpController', function($scope, $http) {
-    $scope.dateOptions = {
-        dateDisabled: disabled,
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        startingDay: 1
-    };
+     function initData() {
+
+        $scope.user = {};
+        $scope.dateOptions = {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            startingDay: 1
+        };
+
+        $http.get('/location')
+            .then(function(response) {
+                $scope.countrylist = response.data.countrylist;
+                $scope.user.national_id = $scope.countrylist[0].id;
+                $scope.user.country_id = $scope.countrylist[0].id;
+
+                $scope.onSelectCountry($scope.user.country_id);
+            }).catch(function(response) {
+            })
+            .finally(function() {
+            });
+    }
+
+    initData();
+
+    $scope.onSelectCountry = function(country_id) {
+        $http.get('/city/' + country_id)
+            .then(function(response) {
+                $scope.citylist = response.data.citylist;
+                $scope.user.city_id = $scope.citylist[0].id;
+             }).catch(function(response) {
+            })
+            .finally(function() {
+            });
+    }
 
     $scope.open = function() {
         $scope.opened = true;
@@ -21,4 +50,6 @@ app.controller('SignUpController', function($scope, $http) {
         //return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
         return false;
     }
+
+
 });
